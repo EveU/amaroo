bb7d5e7232785a950b5fba1f93b84d4b76eb5279
Gary Carmichael <g.carmichael90@gmail.com>
2016-02-12 16:42:00 +0000

    Added display of hotels and flight data to index.

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,34 @@
+var sampleData = require('./sample');
+
+var displayFlights = function(){
+  for(flight of sampleData.flights){
+    var flightsDiv = document.getElementById("allFlights");
+
+    var flightsDisplay = document.createElement("p")
+    flightsDisplay.innerHTML = "Departure: " + flight.departure + "<br>Departure Time: " + flight.departing + "<br><br> Arrival: " + flight.arrival + "<br>Arrival Time: " + flight.arriving + "<br><br>Price: £" + flight.price + "<br><br><hr>";
+
+    flightsDiv.appendChild(flightsDisplay);
+  }
+};
+
+var displayHotels = function(){
+  for(hotel of sampleData.hotels){
+    var hotelsDiv = document.getElementById("allHotels");
+
+    var hotelsDisplay = document.createElement("p");
+    hotelsDisplay.innerHTML = "<b>" + hotel.name + "</b><br>Price Per Person: " + hotel.pricePerPerson + "<br>Number of Rooms: " + hotel.rooms + "<br>Rating: " + hotel.stars + " Stars + <hr>";
+
+    hotelsDiv.appendChild(hotelsDisplay);
+  }
+};
+
 window.onload = function(){
   console.log('loaded app');
+  console.log('sampleData', sampleData.hotels);
+
+  displayFlights();
+  displayHotels();
+
 };
+
+// Number(flight.price).toFixed(2)
