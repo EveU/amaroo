8fcd42869c713aaa36d6e9ed9a2782fa1f22746c
Gary Carmichael <g.carmichael90@gmail.com>
2016-02-15 13:27:55 +0000

    Moved display methods from view to logic. Refactored display methods to allow for display of all or selected flights based on object passed as argument.

--- client/src/booking/BookingOptions.js
+++ client/src/booking/BookingOptions.js
@@ -1,4 +1,5 @@
 var _ = require('lodash');
+var sampleData = require('./../sample');
 
 var BookingOptions = function(){
   this.bookingType = "";
@@ -29,6 +30,26 @@ BookingOptions.prototype = {
     this.hotelsInCity = _.filter(this.hotels, ['address.city', city]);
     this.hotelsInCity = _.orderBy(this.hotelsInCity, ['pricePerPerson'], ['asc']);
   },
+  displayFlights: function(object){
+    for(flight of object){
+      var flightsDiv = document.getElementById("allFlights");
+
+      var flightsDisplay = document.createElement("p")
+      flightsDisplay.innerHTML = "Departure: " + flight.departure + "<br>Departure Time: " + flight.departing + "<br><br> Arrival: " + flight.arrival + "<br>Arrival Time: " + flight.arriving + "<br><br>Price: £" + flight.price + "<br><hr>";
+
+      flightsDiv.appendChild(flightsDisplay);
+    }
+  },
+  displayHotels: function(object){
+    for(hotel of object){
+      var hotelsDiv = document.getElementById("allHotels");
+
+      var hotelsDisplay = document.createElement("p");
+      hotelsDisplay.innerHTML = "<b>" + hotel.name + "</b><br>Price Per Person: £" + hotel.pricePerPerson + "<br>Number of Rooms: " + hotel.rooms + "<br>Rating: " + hotel.stars + " Stars <hr>";
+
+      hotelsDiv.appendChild(hotelsDisplay);
+    }
+  }
 };
 
 module.exports = BookingOptions;
--- client/src/index.js
+++ client/src/index.js
@@ -1,33 +1,27 @@
 var sampleData = require('./sample');
-
-var displayFlights = function(){
-  for(flight of sampleData.flights){
-    var flightsDiv = document.getElementById("allFlights");
-
-    var flightsDisplay = document.createElement("p")
-    flightsDisplay.innerHTML = "Departure: " + flight.departure + "<br>Departure Time: " + flight.departing + "<br><br> Arrival: " + flight.arrival + "<br>Arrival Time: " + flight.arriving + "<br><br>Price: £" + flight.price + "<br><br><hr>";
-
-    flightsDiv.appendChild(flightsDisplay);
-  }
-};
-
-var displayHotels = function(){
-  for(hotel of sampleData.hotels){
-    var hotelsDiv = document.getElementById("allHotels");
-
-    var hotelsDisplay = document.createElement("p");
-    hotelsDisplay.innerHTML = "<b>" + hotel.name + "</b><br>Price Per Person: " + hotel.pricePerPerson + "<br>Number of Rooms: " + hotel.rooms + "<br>Rating: " + hotel.stars + " Stars <hr>";
-
-    hotelsDiv.appendChild(hotelsDisplay);
-  }
-};
+var BookingOptions = require('./booking/BookingOptions');
 
 window.onload = function(){
   console.log('loaded app');
   console.log('sampleData', sampleData.hotels);
+  var booking = new BookingOptions();
 
-  displayFlights();
-  displayHotels();
+  for(flight of sampleData.flights){
+    booking.addFlight(flight);
+  }
+  for(hotel of sampleData.hotels){
+    booking.addHotel(hotel);
+  }
+
+  booking.matchingFlights('28-03-2016 T10:00:00');
+  booking.matchingHotels("Canberra");
+
+
+  booking.displayFlights(booking.flights);
+  booking.displayHotels(booking.hotels);
+  
+  // booking.displayFlights(booking.flightsOnDate);
+  // booking.displayHotels(booking.hotelsInCity);
 
 };
 
